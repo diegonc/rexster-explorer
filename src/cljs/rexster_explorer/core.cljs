069e0b3c00d99161e18d7088f676a0b98a2ab979
Diego Nieto Cid <dnietoc@gmail.com>
2015-11-18 00:36:20 -0300

    UI: add header with graph information

--- src/cljs/rexster_explorer/core.cljs
+++ src/cljs/rexster_explorer/core.cljs
@@ -2,8 +2,27 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om-tools.dom :as dom :include-macros true]
+            [om-tools.core :refer-macros [defcomponent]]
             [cljs.core.async :refer [<!]]
             [rexster-explorer.http-rexster-graph :as rexster]
             [rexster-explorer.rexster-graph :as rg]))
 
 (enable-console-print!)
+
+(defonce app-state
+  (atom {:current-graph "tinkergraph"
+         "tinkergraph" {:graph (rexster/make-graph "192.168.1.6:8182" "tinkergraph")}}))
+
+(defcomponent graph-information [data owner]
+  (render [_]
+    (let [graph (-> data :current-graph data :graph)
+          graph-name (rexster/get-graph-name graph)
+          graph-uri (rexster/get-graph-uri graph)]
+      (dom/div
+       (dom/h1 graph-name)
+       (dom/h6 graph-uri)))))
+
+(om/root
+  graph-information
+  app-state
+  {:target (. js/document (getElementById "graph-info"))})
